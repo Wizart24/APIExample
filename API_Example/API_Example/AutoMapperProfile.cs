366f56aa9d184dfbf9e83d87c79fd95d69dc0e51
Wizart24 <deivas24@gmail.com>
2023-07-01 18:04:31 +0300

    First Commit

---
+++ API_Example/API_Example/AutoMapperProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Example
+{
+	public class AutoMapperProfile : Profile
+	{
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
