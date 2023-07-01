8144ae6e70d4ee50c9bb73d995662764c44f1a83
Wizart24 <deivas24@gmail.com>
2023-07-01 21:24:50 +0300

    Second Commit

--- API_Example/API_Example/Program.cs
+++ API_Example/API_Example/Program.cs
@@ -2,8 +2,12 @@ global using API_Example.Models;
 global using API_Example.Services.CharacterService;
 global using API_Example.Dtos.Character;
 global using AutoMapper;
-
-
+global using Microsoft.EntityFrameworkCore;
+global using API_Example.Data;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.OpenApi.Models;
 
 namespace API_Example
 {
@@ -15,12 +19,37 @@ namespace API_Example
 
 			// Add services to the container.
 
+			builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+			builder.Services.AddSwaggerGen(c =>
+			{
+				c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+				{
+					Description = """Standart Authorization header using the Bearer scheme. Example: "bearer {token}" """,
+					In = ParameterLocation.Header,
+					Name = "Authorization",
+					Type = SecuritySchemeType.ApiKey
+				});
+
+				c.OperationFilter<SecurityRequirementsOperationFilter>();
+			});
 			builder.Services.AddAutoMapper(typeof(Program).Assembly);
 			builder.Services.AddScoped<ICharacterService, CharacterService>();
+			builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+							.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+						ValidateIssuer = false,
+						ValidateAudience = false
+					};
+				});
 
 			var app = builder.Build();
 
@@ -33,6 +62,7 @@ namespace API_Example
 
 			app.UseHttpsRedirection();
 
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 
--- API_Example/API_Example/Services/CharacterService/ICharacterService.cs
+++ API_Example/API_Example/Services/CharacterService/ICharacterService.cs
@@ -3,7 +3,7 @@ namespace API_Example.Services.CharacterService
 {
 	public interface ICharacterService
 	{
-		Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+		Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters(int userId);
 		Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
 		Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
 		Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
